3d727b40d35e7c183149b895eb15d80fd8918ada
Deepanshu Mishra <d4deepanshu723@gmail.com>
2024-09-18 22:03:46 +0530

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -3,25 +3,23 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
-  const token = await getToken({
-    req: req,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
-
-  const publicPaths = path === "/" || path === "/api/auth/signin" || path === "/protected";
+  const token = await getToken({ req });
+  const publicPaths =
+    path === "/" || path === "/api/auth/signin" || path === "/protected";
 
   if (publicPaths && token) {
     return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
   }
 
-  if(path === "/dashboard" && !token){
+  if (path === "/dashboard" && !token) {
     return NextResponse.redirect(new URL("/api/auth/signin", req.nextUrl));
   }
+
   if (!publicPaths && !token) {
     return NextResponse.redirect(new URL("/protected", req.nextUrl));
   }
 }
 
 export const config = {
-  matcher: ["/api/auth/signin", "/dashboard", "/protected"],
+  matcher: ["/api/auth/signin", "/dashboard", "/protected", "/"],
 };
